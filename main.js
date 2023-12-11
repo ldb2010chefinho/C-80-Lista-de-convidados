2db9624c5d839ac812859cafcf167c7f0c0a391c
ldb2010chefinho <128753023+ldb2010chefinho@users.noreply.github.com>
2023-12-11 10:03:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var namesOfPeople = [];
+function submit(){
+var GuestName = document.getElementById("name1").value;
+namesOfPeople.push("GuestName");
+namesOfPeople.sort();
+console.log(GuestName);
+console.log(namesOfPeople);
+var lenghtOfName = nameOfPeople.length;
+console.log(lenghtOfName);
+document.getElementById("displayName").innerHTML=nameOfPeople.toString();
+};
+
+function sorting() {
+    namesOfPeople.sort();
+    var i= namesOfPeople.join("<br>");
+    console.log(namesOfPeople);
+    document.getElementById("sorted").innerHTML=i.toString();
+};
+
+function show() {
+    var i= namesOfPeople.join("<br>");
+    console.log(namesOfPeople);
+    document.getElementById("p1").innerHTML=i.toString();
+    document.getElementById("sortButton").style.display="block";
+};
+
+function searching(){
+    var s =document.getElementById("s1").value;
+    var found=0;
+    var j;
+    for (j=0; j<namesOfPeople; j++);
+    {
+    if(s==namesOfPeople[j]);{
+        found=found+1
+    }
+    }
+    document.getElementById("p2").innerHTML="Nome encontardo"+found+" vez(es)";
+    console.log("Nome encontrado "+found+"vez(es)")
+}
